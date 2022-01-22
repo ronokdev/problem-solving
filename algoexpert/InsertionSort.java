b9b31bf7fc6e29bc626f4d42118aafd3c0d1ad72
fuad.ronok@gmail.com <fuad.ronok@gmail.com>
2022-01-22 21:15:41 +0600

    fix(Insertion-sort): comment updated

--- algoexpert/InsertionSort.java
+++ algoexpert/InsertionSort.java
@@ -13,14 +13,14 @@ public class InsertionSort {
         /*
          * in insertion sort we will always think that the 0th index (First element) is always sorted
          *
-         * Steps:
-         *  write a FOR loop for iterating through the arr
-         *  we will call this iterating "passes"
+         * - Steps:
+         *  - write a FOR loop for iterating through the arr
+         *  - we will call this iterating "passes"
          *
-         *  the for loop will start from 1 index of the arr
-         *   we will get the value(v) a specific index and
-         *  check which value is smaller than v
-         *  we will do this checking via a WHILE loop
+         *  - the for loop will start from 1 index of the arr
+         *    - we will get the value(el) a specific index and
+         *  - check which value in the arr is smaller than el
+         *    - we will do this checking inside a WHILE loop
          *
          * Time complexity  : O(n^2)
          * Space complexity : O(1)
@@ -31,16 +31,20 @@ public class InsertionSort {
         int j = 0;
         for (int i = 1; i < arr.length; i++) {
 
+            // getting the value of the index
             el = arr[i];
+
+            // getting the previous index of i
             j = i - 1;
 
-            // Here we are in need of the j > -1 logic because, we are decrementing j and some point it will be '-1'
+            // Here we are in need of the j > -1 logic because, we are decrementing j and some point j will be '-1'
             while (j > -1 && el < arr[j]) {
 
                 arr[j + 1] = arr[j];
                 j--;
             }
 
+            // if we found a value smaller than el then we will insert the `el` at j+1
             arr[j + 1] = el;
         }
         return arr;
