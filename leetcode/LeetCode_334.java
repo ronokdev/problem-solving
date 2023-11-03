3e082d2e2914a69681da567baa7ec7f9f8a5cd77
Farhan <farhan.ronok@elinvar.de>
2023-11-03 12:18:51 +0100

    leet code 334 with and without brute force approach

---
+++ leetcode/LeetCode_334.java
@@ -0,0 +1,54 @@
+package com.ronok.leetcode;
+
+public class LeetCode_334 {
+    public static void main(String[] args){
+
+        System.out.println("with brute force");
+        System.out.println(increasingTripletBruteForce(new int[]{1,2,3,4,5})); // true
+        System.out.println(increasingTripletBruteForce(new int[]{5,4,3,2,1})); // false
+        System.out.println(increasingTripletBruteForce(new int[]{2,1,5,0,4,6})); // true
+        System.out.println(increasingTripletBruteForce(new int[]{2})); // false
+        System.out.println(increasingTripletBruteForce(new int[]{2,2})); // false
+        System.out.println(increasingTripletBruteForce(new int[]{1,1})); // false
+        System.out.println(increasingTripletBruteForce(new int[]{1,1,2})); // false
+        System.out.println(increasingTripletBruteForce(new int[]{20,100,10,12,5,13})); // true
+
+        System.out.println("without brute force");
+        System.out.println(increasingTriplet(new int[]{1,2,3,4,5})); // true
+        System.out.println(increasingTriplet(new int[]{5,4,3,2,1})); // false
+        System.out.println(increasingTriplet(new int[]{2,1,5,0,4,6})); // true
+        System.out.println(increasingTriplet(new int[]{2})); // false
+        System.out.println(increasingTriplet(new int[]{2,2})); // false
+        System.out.println(increasingTriplet(new int[]{1,1})); // false
+        System.out.println(increasingTriplet(new int[]{1,1,2})); // false
+        System.out.println(increasingTriplet(new int[]{20,100,10,12,5,13})); // true
+    }
+
+    public static boolean increasingTripletBruteForce(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                for (int k = j+1; k < nums.length; k++) {
+                    if(nums[i] < nums[j] && nums[j] < nums[k] )
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean increasingTriplet(int[] nums) {
+        var first = Integer.MAX_VALUE;
+        var second = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= first)
+                first = num;
+            else if (num <= second)
+                second = num;
+            else
+                return true;
+        }
+        return false;
+    }
+
+}
